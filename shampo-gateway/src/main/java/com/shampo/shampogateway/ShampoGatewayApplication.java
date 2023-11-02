0451f1b0f59d7639044a1cc9c65a960db3c57597
larrylai <109332899+Shampohoe@users.noreply.github.com>
2023-11-02 17:49:19 +0800

    6.1、引入Dubbo以便实现远程方法调用 6.2、设置Nacos作为服务注册中心

--- shampo-gateway/src/main/java/com/shampo/shampogateway/ShampoGatewayApplication.java
+++ shampo-gateway/src/main/java/com/shampo/shampogateway/ShampoGatewayApplication.java
@@ -1,13 +1,42 @@
 package com.shampo.shampogateway;
 
+import com.shampo.project.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
+@EnableDubbo
+@Service
 public class ShampoGatewayApplication {
 
+	@DubboReference
+	private DemoService demoService;
 	public static void main(String[] args) {
-		SpringApplication.run(ShampoGatewayApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(ShampoGatewayApplication.class, args);
+		ShampoGatewayApplication application = context.getBean(ShampoGatewayApplication.class);
+		String result = application.doSayHello("world");
+		String result2 = application.doSayHello2("world");
+		System.out.println("result: " + result);
+		System.out.println("result: " + result2);
+	}
+
+	public String doSayHello(String name) {
+		return demoService.sayHello(name);
+	}
+
+	public String doSayHello2(String name) {
+		return demoService.sayHello2(name);
 	}
 
 
--- src/main/java/com/shampo/project/MainApplication.java
+++ src/main/java/com/shampo/project/MainApplication.java
@@ -1,5 +1,6 @@
 package com.shampo.project;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,14 +12,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  *
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication()
+@SpringBootApplication
 @MapperScan("com.shampo.springbootinit.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableDubbo
 public class MainApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MainApplication.class, args);
+
     }
 
 }
